1a2c6335e1898078fe5eae57475578a424252698
RohMeOh <romeogr21@gmail.com>
2026-04-07 07:28:04 -0400

    Spacing

--- binary_trees.h
+++ binary_trees.h
@@ -26,26 +26,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-
-/**
- * binary_tree_postorder - Goes through a binary tree using post-order traversal
- * @tree: Pointer to the root node
- * @func: Function to call for each node
- */
+void binary_tree_preorder(const binary_tree_t *tree, 
+		void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, 
+		void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree,
-	void (*func)(int))
-{
-	if (tree == NULL || func == NULL)
-		return;
-
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-
-	func(tree->n);
-}
-
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
