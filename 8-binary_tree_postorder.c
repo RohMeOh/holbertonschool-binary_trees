14e343e3e4e5b726944ea24d97818b0d96ab2a7e
RohMeOh <romeogr21@gmail.com>
2026-04-07 07:35:08 -0400

    Formatting

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,13 +5,13 @@
  * @tree: Pointer to the root node
  * @func: Function to call for each node
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree,
+	void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
-
 	func(tree->n);
 }
